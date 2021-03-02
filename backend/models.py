15ca2a771a0ac778c3e9ade8fe3e714a0085f27c
Lennard Beers <lennard.beers@rub.de>
2021-03-02 12:10:31 +0100

    lobby-user relationship WIP

--- backend/models.py
+++ backend/models.py
@@ -6,30 +6,32 @@ class Word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(32), index=True)
 
-class WordSchema(ModelSchema):
-    class Meta:
-        model = Word
-        sqla_session = db.session
+#a lobby contains multiple users, a user is child of one lobby
+#running python ./backend/build_database.py gives sqlalchemy.exc.AmbiguousForeignKeysError
 
 class Lobby(db.Model):
     __tablename__ = "lobbies"
     id = db.Column(db.Integer, primary_key=True)
-    users = db.relationship("User", back_populates="lobbies")
+    users = db.relationship("User", backref="lobby") #One-To-Many relationship
     owner = db.Column(db.Integer, db.ForeignKey("users.id"))
     token = db.Column(db.String(16), index=True)
-    
-class LobbySchema(ModelSchema):
-    class Meta:
-        model = Lobby
-        sqla_session = db.session
 
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True)
     token = db.Column(db.String(16), index=True)
-    lobby_id = db.Column(db.Integer, db.ForeignKey("item.id"))
-    lobby = db.relationship("Lobby", back_populates="users")
+    lobby_id = db.Column(db.Integer, db.ForeignKey("lobbies.id")) #backref foreign key column
+    
+class WordSchema(ModelSchema):
+    class Meta:
+        model = Word
+        sqla_session = db.session
+
+class LobbySchema(ModelSchema):
+    class Meta:
+        model = Lobby
+        sqla_session = db.session
 
 class UserSchema(ModelSchema):
     class Meta:
