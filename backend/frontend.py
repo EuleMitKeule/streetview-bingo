4396921f691e30bb673111fc7daf19691e63a1ec
Lennard Beers <lennard.beers@rub.de>
2021-03-18 22:36:18 +0100

    Update frontend.py

--- backend/frontend.py
+++ backend/frontend.py
@@ -1,5 +1,6 @@
 """ Module that includes methods serving the Angular-frontend files """
 from flask import send_from_directory, current_app
+from os import path
 
 
 @current_app.route("/<path:filename>", methods=["GET"])
@@ -7,6 +8,8 @@ def serve_file(filename: str):
     content_type = 'application/javascript'
     if filename == "index.html":
         content_type = 'text/html'
+    if path.splitext(filename)[-1].lower() == ".css":
+        content_type = "text/css"
     return send_from_directory("../frontend/dist/frontend/", filename), {'content-type': content_type}
 
 
